96cb9cc72358d31a3651d5144a320e559df86e65
Colby Sullivan <88957296+colbySullivan@users.noreply.github.com>
2023-06-07 14:03:07 -0500

    Added more csv and fixed reader

--- DCU.py
+++ DCU.py
@@ -4,31 +4,24 @@ import pandas as pd
 import pdfkit
 import os
 
-def addToPDF(html_file, pdf_file, config):
-    pdf_directory = 'pdfFolder'
-    os.path.join(pdf_directory, filename)
-    pdfkit.from_string(html_file, pdf_file, configuration=config)
-        # checking if it is a file
-        #if os.path.isfile(f):
-           
-
 def convert():
     csv_directory = 'csvFolder'
-    pdf_directory = 'pdfFolder'
 
     # iterate over files in
     # that directory
     for filename in os.listdir(csv_directory):
-        f = os.path.join(csv_directory, filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            df1 = pd.read_csv(f)
-            html_file = df1.to_html()
-            path_wkhtmltopdf = r'wkhtmltopdf\bin\wkhtmltopdf.exe'
-            config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-            buffer = f[:-3]
-            pdf_file = buffer + "pdf"
-            addToPDF(html_file, pdf_file, config)
+        if filename.endswith('.csv'):
+            f = os.path.join(csv_directory, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                df1 = pd.read_csv(f)
+                html_file = df1.to_html()
+                path_wkhtmltopdf = r'wkhtmltopdf\bin\wkhtmltopdf.exe'
+                config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+                print(f)
+                buffer = f[:-3]
+                pdf_file = buffer + "pdf"
+                pdfkit.from_string(html_file, pdf_file, configuration=config)
 
 def main():
     convert()
