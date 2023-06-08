ea2f757ee54cb08fc08a2d160091187c66e5a52a
Colby Sullivan <88957296+colbySullivan@users.noreply.github.com>
2023-06-08 09:06:36 -0500

    Cleaned up for multiprocessing and HTML

--- DCU.py
+++ DCU.py
@@ -5,10 +5,15 @@ import pdfkit
 import os
 import csv
 import sys
-import getopt
-import argparse
 
 def check_duplicates():
+    """
+    Traverses through given CSV file and puts all
+    PDF files into a list so that they can be compared
+    in convert()
+
+    :return: list of PDF files without their extension type
+    """ 
     current_files = [] #list of current pdf files to skip
     csv_directory = 'csvFolder'
     for filename in os.listdir(csv_directory):
@@ -18,34 +23,64 @@ def check_duplicates():
     return current_files
 
 
-def convert(processed_count, process_type):
-    csv_directory = 'csvFolder'
+def create_html(f):
+    """
+    Takes in a CSV file and converts it into
+    an HTML file
 
-    # iterate over files in
-    # that directory
+    :param f: current csv file
+    :return: converted html file
+    """ 
+    df1 = pd.read_csv(f)
+    return df1.to_html() #Converts file to html
+    #TODO we can edit the html here
+
+def create_pdf(current_file, html_file):
+    """
+    Takes in an HTML file and converts it into
+    a PDF file
+
+    :param current_file: string name of CSV file
+    :param html_file: current HTML file
+    """ 
+    print(current_file)
+    path_wkhtmltopdf = r'wkhtmltopdf\bin\wkhtmltopdf.exe' #Dependency needed for pdfkit
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
+    #Formats the csv to pdf
+    buffer = current_file[:-3]
+    pdf_file = buffer + "pdf"
+    pdfkit.from_string(html_file, pdf_file, configuration=config)  #Utilizes the pdfkit API to convert the html to a pdf
+
+
+def convert(processed_count, file_path, process_type):
+    """
+    Traverses through a given directory and searches 
+    for CSV files. For each file they are converted
+    into PDF files based on the given process type.
+
+    :param processed_count: counts file conversions
+    :param file_path: given path to the CSV folder
+    :param process_type: Smart, Abort, or Process
+    :return: integer of files processed
+    """ 
+    #file_path = 'csvFolder'
+
+    #Checks for reprocess and smart
     if process_type == 'reprocess':
-        print("reprocess")
         current_files = []
     else:
-        print("smart")
         current_files = check_duplicates()
-    for filename in os.listdir(csv_directory):
+
+    # iterate over files in the given directory path
+    for filename in os.listdir(file_path):
         buffer_filename = filename[:-3]
         if buffer_filename not in current_files:
             if filename.endswith('.csv'): #Ignores pdf files
-                f = os.path.join(csv_directory, filename)
-                # checking if it is a file
-                if os.path.isfile(f):
-                    df1 = pd.read_csv(f)
-                    html_file = df1.to_html() #Converts file to html
-                    path_wkhtmltopdf = r'wkhtmltopdf\bin\wkhtmltopdf.exe' #Dependency needed for pdfit
-                    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-                    print(f) #Debug prints out file name
-                    #Formats the csv to pdf
-                    buffer = f[:-3]
-                    pdf_file = buffer + "pdf"
-                    #Utilies the pdfkit API to convert the html to a pdf
-                    pdfkit.from_string(html_file, pdf_file, configuration=config)
+                f = os.path.join(file_path, filename)
+                if os.path.isfile(f): # checking if it is a file
+                    html_file = create_html(f)
+                    create_pdf(f, html_file)
                     processed_count+=1
         elif process_type == 'abort':
             print("abort")
@@ -80,12 +115,16 @@ def find_missing_services(csv_file):
                                
 class CommandLine:
     def __init__(self):
-        if len(sys.argv) == 2:
-            argument = sys.argv[1]
+        if len(sys.argv) == 3:
+            argument1 = sys.argv[1]
+            argument2 = sys.argv[2]
+        elif len(sys.argv) == 2:
+            argument1 = sys.argv[1]
+            argument2 = 'smart'
         else:
-            argument = 'smart'
+            raise Exception("\nMissing at least one argument \nArgument 1 is the path to the CSV file \nArgument 2 is optional and is the processing type")
         convert_counter = 0 #Keeps track of files that have been converted
-        convert_counter = convert(convert_counter, argument)
+        convert_counter = convert(convert_counter, argument1, argument2)
         print("{}{}".format(convert_counter, ' files have been converted'))
     
     # Usage
